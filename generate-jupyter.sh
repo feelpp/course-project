8f24f9c05db43dbebada9d7fd5ff812cb02c24b3
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2026-03-10 10:41:11 +0100

    Update site.yml to adjust documentation source paths and UI supplemental files
    - Changed the start path for the first source from 'docs' to 'docs/course'.
    - Added a new source with start path 'docs/csmi'.
    - Updated the supplemental files path to './docs/course/antora/supplemental-ui'.

--- generate-jupyter.sh
+++ generate-jupyter.sh
@@ -1,14 +1,26 @@
 #! /bin/sh
 
 dir=${1:-.}
-for i in $(find $dir -name "*.adoc" | grep pages ); do 
-    if grep ":page-jupyter:" $i; then   
-        echo "Generating Jupyter Notebook for $i: "
-        ipynb=$(echo $i |  sed -E 's/docs\/modules\/(.*)\/pages\/(.*)/\1\/\2/')
-        dir=$(dirname $ipynb)
-        mkdir -p notebooks/$dir
-        echo "generating notebooks/$dir/$(basename $ipynb .adoc).ipynb..."
+find "$dir" -name "*.adoc" | grep pages | while IFS= read -r file; do
+    if grep -q ":page-jupyter:" "$file"; then
+        echo "Generating Jupyter Notebook for $file: "
+        ipynb=$(echo "$file" | sed -E 's#^docs/course/modules/([^/]+)/pages/(.*)$#\1/\2#')
+        if [ "$ipynb" = "$file" ]; then
+            ipynb=$(echo "$file" | sed -E 's#^docs/csmi/modules/([^/]+)/pages/(.*)$#\1/\2#')
+        fi
+        if [ "$ipynb" = "$file" ]; then
+            ipynb=$(echo "$file" | sed -E 's#^docs/modules/([^/]+)/pages/(.*)$#\1/\2#')
+        fi
+
+        if [ "$ipynb" = "$file" ]; then
+            echo "Skipping $file: unable to map output path."
+            continue
+        fi
+
+        outdir=$(dirname "$ipynb")
+        mkdir -p "notebooks/$outdir"
+        echo "generating notebooks/$outdir/$(basename "$ipynb" .adoc).ipynb..."
         pwd
-        npx asciidoctor -r asciidoctor-jupyter -b jupyter $i -o notebooks/$dir/$(basename $ipynb .adoc).ipynb
-    fi; 
+        npx asciidoctor -r asciidoctor-jupyter -b jupyter "$file" -o "notebooks/$outdir/$(basename "$ipynb" .adoc).ipynb"
+    fi
 done
